24f42829db841d4d920c673d6b3663f439c78b06
Peter Anning <22272449+peteanning@users.noreply.github.com>
2024-03-20 16:32:27 +0000

    fix replace provider script

--- replace-provider.sh
+++ replace-provider.sh
@@ -4,11 +4,11 @@ environment=$1
 profile=$2
 provider=${3:-aws}
 autoAnswer=${4-no}
-globalAnswerYes=1
+globalAnswer="no"
 autoApprove=""
 
 if [ $autoAnswer = "yes" ];then
-  globalAnswerYes=0;
+  globalAnswer=$autoAnswer;
   autoApprove="-auto-approve"
 fi
 
@@ -18,7 +18,7 @@ cmd="aws-profile -p upscan-labs  terraform state replace-provider $autoApprove -
 
 
 usage(){
-  echo "replace-provider labs "development | qa | staging""
+  echo "replace-provider 'labs | live'  'development | qa | staging' 'provider'  'yes | no'"
   echo "this script runs "$cmd""
 }
 
@@ -33,7 +33,7 @@ done
 
 ask_continue() {
     local message=$1
-    if $globalAnswerYes; then
+    if [ $globalAnswer = "yes" ]; then
       echo "$message: y"
       choice="y"
     else
@@ -48,22 +48,26 @@ ask_continue() {
 }
 
 do_cmd(){
-  local cwd=$(pwd)
-	for dir in "$cwd/components/"*/; do
-    if [ -d "$dir" ]; then
-      (cd $dir
-        if ask_continue "About to run $cmd for $dir";then
-          rm -rf .terraform/
-          aws-profile -p upscan-${environment} terraform init --backend-config=../../backends/${profile}.tfvars
-          aws-profile -p upscan-${environment} terraform workspace select ${profile}
-          $cmd
-        fi)
+  if ask_continue  "Running in $environment";then	
+	  local cwd=$(pwd)
+		for dir in "$cwd/components/"*/; do
+	    if [ -d "$dir" ]; then
+	      (cd $dir
+	        if ask_continue "About to run $cmd for $dir";then
+	          rm -rf .terraform/
+	          aws-profile -p upscan-${environment} terraform init --backend-config=../../backends/${profile}.tfvars
+	          aws-profile -p upscan-${environment} terraform workspace select ${profile}
+	          $cmd
+	        fi)
+	  fi
+	  done
+  else
+	echo "Exiting"
   fi
-  done
 }
 
 
-if [ "$environment" = "labs" ]; then
+if [ "$environment" = "labs" ] || [ "$environment" = "live" ]; then
   if [ "$profile" = "development" ] || [ "$profile" = "qa" ] || [ "$profile" = "staging" ]; then
     do_cmd
   else
