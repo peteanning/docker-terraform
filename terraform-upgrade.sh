4f5a4bfebc51c02c3852b82249041041eb7b1581
Peter Anning <22272449+peteanning@users.noreply.github.com>
2024-03-25 18:07:02 +0000

    updated terraform upgrade

--- terraform-upgrade.sh
+++ terraform-upgrade.sh
@@ -1,20 +1,71 @@
 #!/bin/bash
 
-set -u
+set -ux
 
 upgradeFlag=false
 file="versions.tf"
+cwd=$(pwd)
+profile="UNSET"
+initCmd="UNSET"
+selectProfileCmd="UNSET"
+savePlanCmd="UNSET"
+component="*"
+validComponents=(
+    "clamav"
+    "dns"
+    "ecs_cluster"
+    "ecs_upload_proxy"
+    "ecs_verify"
+    "iam"
+    "nat"
+    "network"
+    "pagerduty_alerts"
+    "s3"
+    "s3_sqs_notification"
+    "security_groups"
+    "sqs"
+    "sqs_policy"
+    "squid"
+    "vpce_artefacts"
+    "vpce_cloudwatch"
+    "vpce_graphite"
+    "vpce_s3"
+    "vpce_sqs"
+    "vpce_telemetry_kafka"
+    "waf_ac"
+)
+
+initialise_commands() {
+  echo "Setting up commands for $environment in the $profile workspace"  
+  awsProfile="aws-profile -p upscan-$environment"	
+  initCmd="$awsProfile terraform init --backend-config=../../backends/$profile.tfvars"
+  selectProfileCmd="$awsProfile terraform workspace select $profile"	
+  savePlanCmd="$awsProfile terraform plan -out tfplan.out"
+  applyCmd="$awsProfile terraform apply tfplan.out"  
+}
 
 upgrade() {
-  echo "Upgrading from $froVersion to $toVersion"
-  for dir in components/*/; do
+  echo "Upgrading from $fromVersion to $toVersion"
+  for c  in "${validComponents[@]}"; do
+    local dir="$cwd/$c"
+
     echo "Looking for $file in $dir"
     cd "$dir" || continue
-    
     if [  -f "$file" ]; then
       echo "Found  $file  in directory: $dir"
-      sed -i 's/>= $fromVersion/>= $toVersion/' $file
+      sed -i "s/>= $fromVersion/>= $toVersion/" $file
     fi
+
+    if [ -d "$dir/.terraform" ]; then
+      echo "Found existing .terraform directory deleting before init"
+      rm -rf "$dir/.terraform/"
+    fi
+
+    echo "Generating the plan after upgrade in tfplan.out"
+    echo "To view the plan use aws-profile -p upscan-$environment terraform show -json tfplan.out"
+    $initCmd
+    $selectProfileCmd
+    $savePlanCmd 
     
     # Navigate back to the parent directory
     cd ..
@@ -22,27 +73,45 @@ upgrade() {
 }
 
 usage() {
-    echo "Usage: $0 [-u|--upgrade] [-f|--from <version>] [-t|--to <version>]"
+    echo "Usage: $0 [-u | --upgrade] [-f | --from <version>] [-t | --to <version>]"
     echo "Options:"
     echo "  -u, --upgrade    Perform upgrade"
     echo "  -f, --from       Specify the version to upgrade from"
     echo "  -t, --to         Specify the version to upgrade to"
 }
 
-while [[ $# -gt 0 ]]; do
-    case $1 in
+for arg in "$@"; do
+    case $arg in
         -u|--upgrade)
             upgradeFlag=true
             shift
             ;;
-        -f|--from)
-            fromVersion=$2
-            shift 2
-            ;;
-        -t|--to)
-            toVersion=$2
-            shift 2
-            ;;
+        --profile=*)
+            profile=${arg#*=}
+	      case $profile in
+		      development|qa|staging)
+			      environment=labs
+			      ;;
+		      externalTest|production)
+			      environment=live
+			      ;;
+		      *)
+			      echo "Invalid Profile $profile"
+			      usage
+			      exit 1
+			      ;;
+	      esac
+
+	    ;;
+        --from=*)
+            fromVersion=${arg#*=}
+	    ;;
+        --to=*)
+            toVersion=${arg#*=}
+	    ;;
+        --component=*)
+            component=${arg#*=}
+	    ;;
         *)
             echo "Error: Unknown option $1"
             usage
@@ -51,13 +120,27 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [ "$upgrade_flag" = true ]; then
-    if [ -z "$from_version" ] || [ -z "$to_version" ]; then
+if [ "$upgradeFlag" = true ]; then
+    if [ -z "$fromVersion" ] || [ -z "$toVersion" ]; then
         echo "Error: Both -f/--from and -t/--to options are required for upgrade"
         usage
         exit 1
     fi
-    upgrade
+    if [[ "$profile" != "development" ]] && [[ "$profile" != "qa" ]] && [[ "$profile" != "staging" ]] && [[ "$profile" != "externalTest" ]] && [[ "$profile" != "production" ]]; then
+       usage
+       exit 1
+    fi
+    initialise_commands
+    if [[ "$component" = "*" ]]; then
+      upgrade
+      exit 0
+    elif [[ " ${validComponents[@]} " =~ " $component " ]]; then
+	    echo "Upgrading $component"
+    else
+	    echo "Invalid $component specified"
+	    echo "Should be one of ${components[@]}"
+	    exit 1
+    fi
 else
     usage
 fi
